34ec496ef8fe01c812bd6b77ab94ee0b1874bbae
BubuDavid <david.pedroza.segoviano@gmail.com>
2023-10-26 15:59:33 -0600

    Add a keybind for cmdline

--- lua/buberto/plugins/fine-cmdline.lua
+++ lua/buberto/plugins/fine-cmdline.lua
@@ -4,6 +4,6 @@ return {
 		"MunifTanjim/nui.nvim"
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>h", "<cmd>FineCmdline<CR>", { desc = "Open fine command line" })
+		vim.keymap.set("n", ":", "<cmd>FineCmdline<CR>", { desc = "Open fine command line", noremap = true})
 	end
 }
