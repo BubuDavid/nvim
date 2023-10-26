8e5f28e05e6304803a0878ff22805c8e278f5690
BubuDavid <david.pedroza.segoviano@gmail.com>
2023-10-26 11:05:31 -0600

    Add "open in current file" config to NeoTree shortcut

--- lua/buberto/plugins/neo-tree.lua
+++ lua/buberto/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
-vim.keymap.set("n", "<leader>tr", ":Neotree toggle float<CR>", {})
+vim.keymap.set("n", "<leader>tr", ":Neotree toggle float reveal<CR>", {})
 
 return {
   "nvim-neo-tree/neo-tree.nvim",
