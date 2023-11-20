5d124e2bd1622707da776513bc80ad692f4620df
BubuDavid <david.pedroza.segoviano@gmail.com>
2023-11-20 12:50:58 -0600

    Configure nvim to be compatible with tmux

--- lua/buberto/core/keymaps.lua
+++ lua/buberto/core/keymaps.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
-local set_keymap = function (mode, input, action, desc, noremap)
-	if (noremap) then
+local set_keymap = function(mode, input, action, desc, noremap)
+	if noremap then
 		vim.keymap.set(mode, input, action, { desc = desc, noremap = noremap })
 	else
 		vim.keymap.set(mode, input, action, { desc = desc })
@@ -20,7 +20,7 @@ set_keymap("v", "J", ":m '>+1<CR>gv=gv", "Move Down the selected text")
 set_keymap("v", "K", ":m '<-2<CR>gv=gv", "Move Up the selected text")
 
 -- Deleting something without copying it
-set_keymap("x", "<leader>p", '\"_dP', "Delete something without copying it")
+set_keymap("x", "<leader>p", '"_dP', "Delete something without copying it")
 set_keymap("n", "<leader>d", '"_d', "Delete something without copying it")
 set_keymap("v", "<leader>d", '"_d', "Delete something without copying it")
 
@@ -30,8 +30,8 @@ set_keymap("n", "<leader>Y", '"+Y', "Copy things from nvim to computer")
 set_keymap("v", "<leader>y", '"+y', "Copy things from nvim to computer")
 
 -- Travel between buffers
-set_keymap({ "n", "i" }, "<C-l>", vim.cmd.bn, "Go to next buffer")
-set_keymap({ "n", "i" }, "<C-h>", vim.cmd.bp, "Go to previous buffer")
+set_keymap({ "n", "i" }, "<tab>", vim.cmd.bn, "Go to next buffer")
+set_keymap({ "n", "i" }, "<S-tab>", vim.cmd.bp, "Go to previous buffer")
 
 -- Delete buffer
 set_keymap("n", "<leader>bd", vim.cmd.bd, "Delete buffer")
@@ -42,4 +42,9 @@ set_keymap("n", "cgn", "*Ncgn")
 set_keymap("n", "g.", [[:/\V\C<C-R>"<CR>cgn<C-A><Esc>]], "cgn but if you forget the cgn", true)
 
 -- Fun replace of under the cursor word globally
-set_keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]], "Replace under the cursor word automatically")
+set_keymap(
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gc<Left><Left><Left>]],
+	"Replace under the cursor word automatically"
+)
--- lua/buberto/core/options.lua
+++ lua/buberto/core/options.lua
@@ -1,6 +1,9 @@
 local opt = vim.opt
 local o = vim.o
 
+-- Add commentary options
+vim.g.skip_ts_context_commentstring_module = true
+
 -- Line Numbers
 opt.relativenumber = true
 opt.number = true
@@ -41,11 +44,11 @@ o.guicursor = true
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
 -- Remove Mouse interactions with Vim
