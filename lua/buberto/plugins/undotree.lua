d7d687d4de368c7c5f0911cf7c0b31f03b7b0080
BubuDavid <david.pedroza.segoviano@gmail.com>
2024-08-28 19:54:54 +0000

    ✨ [ADD] lets take undotree more serious

--- lua/buberto/plugins/undotree.lua
+++ lua/buberto/plugins/undotree.lua
@@ -1,6 +1,8 @@
 return {
 	"mbbill/undotree",
 	config = function()
-		vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>", { desc = "Toggle the Undo Tree" })
-	end
+		vim.keymap.set("n", "<leader>ut", ":UndotreeToggle<CR>", { desc = "Toggle the Undo Tree" })
+		vim.g.undotree_WindowLayout = 2
+		vim.g.undotree_SetFocusWhenToggle = 1
+	end,
 }
