f278876a16c317e3abc0459a986125c39b0b9b14
BubuDavid <david.pedroza.segoviano@gmail.com>
2024-09-06 18:36:08 +0000

    🏳️‍🌈 chaning colorscheme and lazy loading copilot, disabled by default

--- lua/buberto/plugins/colorscheme.lua
+++ lua/buberto/plugins/colorscheme.lua
@@ -1,23 +1,36 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
+	color = color or "rose-pine"
+	vim.cmd([[colorscheme ]] .. color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
 	{
 		"rose-pine/neovim",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			ColorMyPencils("rose-pine-moon")
+			-- ColorMyPencils("rose-pine-moon")
 		end,
 	},
 
 	{
 		"folke/tokyonight.nvim",
+		lazy = true,
+		priority = 1000,
 		config = function()
 			-- ColorMyPencils("tokyonight")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			ColorMyPencils("catppuccin-macchiato")
 		end,
 	},
 }
