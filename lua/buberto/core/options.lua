beb147ac6f2487c138254248c66e89bece9ea863
BubuDavid <david.pedroza.segoviano@gmail.com>
2024-11-13 21:19:16 +0000

    🙌 [REMOVE] Color column from neotree

--- lua/buberto/core/options.lua
+++ lua/buberto/core/options.lua
@@ -70,7 +70,7 @@ remove_colorcolumn()
 
 -- Remove colocolumn for avante
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "Avante*", "json", "conf"},
+	pattern = { "Avante*", "json", "conf", "neo-tree"},
 	callback = remove_colorcolumn,
 })
 
