0043054a03e3b2be314bfbf875fff19f636c71ec
BubuDavid <david.pedroza.segoviano@gmail.com>
2025-03-29 17:19:16 -0600

    [FIX] Isort and Black order

--- lua/buberto/plugins/lsp/lspconfig.lua
+++ lua/buberto/plugins/lsp/lspconfig.lua
@@ -66,7 +66,6 @@ return {
 				python = {
 					analysis = {
 						diagnosticMode = "openFilesOnly",
-						reportUnusedVariable = "none", -- This should disable the specific diagnostic
 					},
 				},
 			},
