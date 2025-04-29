ec40f9c27676a39187eca6414d72f71738148ba8
BubuDavid <david.pedroza.segoviano@gmail.com>
2025-04-29 11:25:45 -0600

    [add] hide notification keymap

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -20,5 +20,6 @@ return {
 	init = function()
 		vim.g.db_ui_use_nerd_fonts = 1
 		vim.keymap.set("n", "<leader>D", ":DBUIToggle<CR>", { desc = "Toggle dbui panel" })
+		vim.keymap.set("n", "<leader>dh", ":DBUIHideNotifications<CR>", { desc = "Hide notification" })
 	end,
 }
